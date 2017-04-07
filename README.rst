a927db53d54cd19c3decb34ea728c5e6d21eed91
Warren Moore <warren@wamonite.com>
2017-04-07 23:00:26 +0100

    Added package setup.

---
+++ README.rst
@@ -0,0 +1,27 @@
+envassume
+=========
+
+Assume an AWS IAM role from environment variables.
+
+To Do
+-----
+
+- Documentation
+- Tests
+
+License
+-------
+
+Copyright (c) 2017 Warren Moore
+
+This software may be redistributed under the terms of the MIT License.
+See the file LICENSE for details.
+
+Contact
+-------
+
+::
+
+          @wamonite     - twitter
+           \_______.com - web
+    warren____________/ - email
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as file_object:
+    readme = file_object.read()
+
+setup(
+    name = 'envassume',
+    version = '0.1',
+    description = 'Assume an AWS IAM role from environment variables',
+    long_description = readme,
+    license = 'MIT',
+    author = 'Warren Moore',
+    author_email = 'warren@wamonite.com',
+    url = 'https://github.com/wamonite/envassume',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ],
+    packages = ['envassume'],
+    entry_points = dict(console_scripts = ['envassume=envassume.main:run_script']),
+    install_requires = [
+        'boto3'
+    ],
+    zip_safe = False
+)
