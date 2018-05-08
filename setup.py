72eb1256084ad99a0f55b6536f1fa0419b05862c
requires.io <support@requires.io>
2018-05-08 03:00:32 +0100

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     packages = ['envassume'],
     entry_points = dict(console_scripts = ['envassume=envassume.main:run']),
     install_requires = [
-        'boto3==1.7.14',
+        'boto3==1.7.15',
         'attrs==18.1.0'
     ],
     zip_safe = False
