ab4376adfd3136b650df6dccfcd1fe36b0420da6
Warren Moore <warren@wamonite.com>
2019-02-21 11:24:23 +0000

    Fix tests for Python 3.

--- tests/test_usage.py
+++ tests/test_usage.py
@@ -4,12 +4,16 @@ from __future__ import print_function
 import pytest
 import mock
 from envassume.usage import print_help, print_error
+import sys
 
 
 @pytest.mark.parametrize('short', (True, False))
 def test_help(short, monkeypatch):
     print_func = mock.Mock()
-    monkeypatch.setattr('__builtin__.print', print_func)
+    if sys.version_info[0] >= 3:
+        monkeypatch.setattr('builtins.print', print_func)
+    else:
+        monkeypatch.setattr('__builtin__.print', print_func)
     print_help(short)
     calls = print_func.call_args_list
     assert calls[0][0][0].startswith('usage:')
@@ -18,7 +22,10 @@ def test_help(short, monkeypatch):
 @pytest.mark.parametrize('message', ('message', 'foo'))
 def test_error(message, monkeypatch):
     print_func = mock.Mock()
-    monkeypatch.setattr('__builtin__.print', print_func)
+    if sys.version_info[0] >= 3:
+        monkeypatch.setattr('builtins.print', print_func)
+    else:
+        monkeypatch.setattr('__builtin__.print', print_func)
     print_error(message)
     calls = print_func.call_args_list
     assert calls[0][0][0].startswith('usage:')
