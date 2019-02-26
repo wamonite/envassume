ebe02e4df15adfc9e458e00bbacb152d2cdb0d8e
Warren Moore <warren@wamonite.com>
2019-02-26 21:43:07 +0000

    Bump version: 1.0.5 → 1.0.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as file_object:
 
 setup(
     name = 'envassume',
-    version = '1.0.5',
+    version = '1.0.6',
     description = 'Assume an AWS IAM role and execute a command with the assumed credentials',
     long_description = readme,
     license = 'MIT',
